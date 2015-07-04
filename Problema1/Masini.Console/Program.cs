9e05cc0a7dc5b4173356c5baec1d95eec06feba3
Madalin Plastoi <madalin.plastoi@synergogroup.net>
2015-07-04 22:07:57 +0300

    added first project

---
+++ Problema1/Masini.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Masini.Library;
+
+namespace Masini.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Masina x = new Masina();
+        }
+    }
+}
