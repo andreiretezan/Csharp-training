51575888eb46fa5b0325fc119232ed80a8101a16
Andrei Retezan <andrei.retezan@gmail.com>
2015-07-24 19:10:34 +0300

    afisarea masinilor -> versiune noua

--- Problema1/Masini.Console/Program.cs
+++ Problema1/Masini.Console/Program.cs
@@ -20,15 +20,20 @@ namespace Masini.EXE
             Masina masina4 = new Masina("Opel Antara", 5, Enums.Culori.verde, C2, "ABC8461DF410000");
 
             Categorie C3 = new Categorie("coupe", 1600, 1580);
-            Masina masina5 = new Masina("Audi TT", 3, Enums.Culori.rosu, C3, "ABC6545KJ540000");
+            Masina masina5 = new Masina("Audi TT", 3, Enums.Culori.rosu, C3, "ABC6545KJ540000");          
 
-            Console.WriteLine(masina1.ToString()); Console.WriteLine();
-            Console.WriteLine(masina2.ToString()); Console.WriteLine();
-            Console.WriteLine(masina3.ToString()); Console.WriteLine();
-            Console.WriteLine(masina4.ToString()); Console.WriteLine();
-            Console.WriteLine(masina5.ToString()); Console.WriteLine();
+            List<Masina> masini = new List<Masina>
+            {
+                masina1, masina2, masina3, masina4, masina5
+            };
 
-            Console.WriteLine("Alegeti una din urmatoarele categorii:C1, C2, C3");
+            foreach (var masina in masini)
+            {
+
+                Console.WriteLine(masina.ToString()); Console.WriteLine();
+            }
+          
+            /*Console.WriteLine("Alegeti una din urmatoarele categorii:C1, C2, C3");
             string alegere = Console.ReadLine();
             if (alegere=="C1" || alegere=="c1")
             {
@@ -47,16 +52,16 @@ namespace Masini.EXE
             else
             {
                 Console.WriteLine("Categoria aleasa nu exista, va rugam incercati din nou");
-            }
+            }*/
 
             /*List<string> nume = new List<string>();
             nume.Add("");
             nume.Add("");
             foreach (var n in nume) 
             {
-                System.Console.WriteLine(n);
-            }*/
-
+                Console.WriteLine(n);
+            }
+            */
             Console.ReadLine();
         }
     }
